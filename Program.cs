3fd8db7437006afe15e12946235c42e6bd83eb63
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-09-24 20:40:20 +0200

    Vuelvo atrás con mi decisión

--- Program.cs
+++ Program.cs
@@ -38,7 +38,10 @@ postre1.MostrarDetalles();
 
 //Producto producto1 = new Producto("Entrante",15m, ["Lechuga, carne"] );
 
-List<Producto> combo = [platoPrincipal1, bebida1, postre1];
+List<Producto> combo = new ();// = [platoPrincipal1, bebida1, postre1];
+combo.Add(platoPrincipal1);
+combo.Add(bebida1);
+combo.Add(postre1);
 
 
 Console.WriteLine(combo);
